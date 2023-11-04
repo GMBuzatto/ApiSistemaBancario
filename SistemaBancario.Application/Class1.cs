3a7fe2ea2db1653d76bcc1c3b39e980e7ede5c77
gabriel.buzatto <gabrielmartinbuzatto1999@gmail.com>
2023-11-04 19:31:33 -0300

    adiciona classe de mapeamento DomainToDTOMappingProfile ao projeto Application

--- SistemaBancario.Application/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace SistemaBancario.Application
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ SistemaBancario.Application/Mappings/DomainToDTOMappingProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using SistemaBancario.Application.DTOs;
+using SistemaBancario.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaBancario.Application.Mappings
+{
+    public class DomainToDTOMappingProfile : Profile
+    {
+        public DomainToDTOMappingProfile()
+        {
+            //Método que cria o mapeamento e permite o ReverseMap tanto do Usuario pro UsuarioDTO
+            //quanto do UsuarioDTO pro Usuario
+            CreateMap<Usuario, UsuarioDTO>().ReverseMap();
+        }
+    }
+}
