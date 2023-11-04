8bded7046824aa40725c33e8bd637ce244e3ecfb
gabriel.buzatto <gabrielmartinbuzatto1999@gmail.com>
2023-11-04 20:11:24 -0300

    adiciona injeção de dependia AddInfraStructureSwagger, UseAuthentication e UseAuthorization que adiciona o token de autorização no projeto API

--- SistemaBancario.API/Program.cs
+++ SistemaBancario.API/Program.cs
@@ -7,15 +7,22 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
 
+//builder.Services.AddSwaggerGen();
+// Subistituimos por nosso método AddInfraStrucureSwagger que adiciona o token de autorização
+builder.Services.AddInfraStructureSwagger();
 //método AddInfrastructure que contem nossas injeções de dependencia,
 //nossa string de conexão e migrations
 DependencyInjection.AddInfrastructure(builder);
 
 
 var app = builder.Build();
+
+//Adicionando a injeção de dependencia de autenticação e autorização
+app.UseAuthentication();
+app.UseAuthorization();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
