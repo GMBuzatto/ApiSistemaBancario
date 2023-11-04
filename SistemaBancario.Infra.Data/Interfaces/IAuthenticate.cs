80c90d38afbbda2e0318cbf11fd326e4e67ffa2b
gabriel.buzatto <gabrielmartinbuzatto1999@gmail.com>
2023-11-04 18:57:22 -0300

    adiciona interface IAuthenticate no projeto Infra.Data

---
+++ SistemaBancario.Infra.Data/Interfaces/IAuthenticate.cs
@@ -0,0 +1,23 @@
+﻿using SistemaBancario.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaBancario.Infra.Data.Interfaces
+{
+    public interface IAuthenticate
+    {
+        Task<bool> AuthenticateAsync(string email, string senha);
+
+        Task<bool> UserExists(string email);
+
+        public string GenerateToken(int id, string email);
+
+        // contrato que vai ser sobreescrito para selecionar o usuario e retornar
+
+        public Task<Usuario> GetUserByEmail(string email);
+    }
+}
+
