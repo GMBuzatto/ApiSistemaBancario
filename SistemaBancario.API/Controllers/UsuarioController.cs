aa4c8ecc8662bb73a84847fd0444c99b6979a32c
gabriel.buzatto <gabrielmartinbuzatto1999@gmail.com>
2023-11-04 20:52:30 -0300

    Adiciona Controller Usuario com os endpoints register e login

---
+++ SistemaBancario.API/Controllers/UsuarioController.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SistemaBancario.API.Models;
+using SistemaBancario.Application.DTOs;
+using SistemaBancario.Application.Interfaces;
+using SistemaBancario.Infra.Data.Interfaces;
+
+namespace SistemaBancario.API.Controllers
+{
+    //passando a notationa que informa que essa e uma controladora
+    [ApiController]
+    [Route("api/[controller]")]// criando a notation com a rota de acesso a controladora
+    public class UsuarioController : Controller
+    {
+
+        //instancia da nossa interface com os métodos IAuthenticate
+        private readonly IAuthenticate _authenticateService;
+        private readonly IUsuarioService _usuarioService;
+
+        public UsuarioController(IAuthenticate authenticateService, IUsuarioService usuarioService)
+        {
+            _authenticateService = authenticateService;
+            _usuarioService = usuarioService;
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<UserToken>> Registrar(UsuarioDTO usuarioDTO)
+        {
+            if (usuarioDTO == null)
+            {
+                return BadRequest("Dados inválidos");
+            }
+
+            var emailExiste = await _authenticateService.UserExists(usuarioDTO.Email);
+
+            if (emailExiste)
+            {
+                return BadRequest("Este e-mail já possui um cadastro.");
+            }
+
+            var usuario = await _usuarioService.Incluir(usuarioDTO);
+            if (usuario == null)
+            {
+                return BadRequest("Ocorreu um erro ao cadastar o usuario");
+            }
+
+            var token = _authenticateService.GenerateToken(usuario.Id, usuario.Email);
+
+            return new UserToken
+            {
+                Token = token
+            };
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<UserToken>> Logar(LoginModel loginModel)
+        {
+            var existe = await _authenticateService.UserExists(loginModel.Email);
+            if (!existe)
+            {
+                return Unauthorized("Usuário não existe.");
+            }
+
+            var result = await _authenticateService.AuthenticateAsync(loginModel.Email, loginModel.Password);
+            if (!result)
+            {
+                return Unauthorized("Usuário ou senha inválido.");
+            }
+
+            var usuario = await _authenticateService.GetUserByEmail(loginModel.Email);
+
+            var token = _authenticateService.GenerateToken(usuario.Id, usuario.Email);
+
+            return new UserToken { Token = token };
+        }
+
+    }
+}
