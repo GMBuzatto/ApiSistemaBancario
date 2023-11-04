45074a6f0c55d008d99f5180444c6d0a4b0c5409
gabriel.buzatto <gabrielmartinbuzatto1999@gmail.com>
2023-11-04 19:34:47 -0300

    adiciona interface IUsuarioService que contem os métodos contratos da classe UsuarioDTO no projeto Application

---
+++ SistemaBancario.Application/Interfaces/IUsuarioService.cs
@@ -0,0 +1,20 @@
+﻿using SistemaBancario.Application.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaBancario.Application.Interfaces
+{
+    //interface com os métodos contratos da classe UsuarioDTO a serem sobreescritos 
+    public interface IUsuarioService
+    {
+        Task<UsuarioDTO> Incluir(UsuarioDTO usuarioDTO);
+        Task<UsuarioDTO> Alterar(UsuarioDTO usuarioDTO);
+        Task<UsuarioDTO> Excluir(int id);
+        Task<UsuarioDTO> SelecionarAsync(int id);
+        Task<IEnumerable<UsuarioDTO>> SelecionarTodosAsync();
+
+    }
+}
