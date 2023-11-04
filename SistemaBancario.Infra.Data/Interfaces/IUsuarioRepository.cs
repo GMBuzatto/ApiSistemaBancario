6012dd96a9ddea19d97b2d1f5c81f912a19235db
gabriel.buzatto <gabrielmartinbuzatto1999@gmail.com>
2023-11-04 17:29:11 -0300

    adiciona interface IUsuarioRepository que adiciona métodos de modificação da classe Usuario

---
+++ SistemaBancario.Infra.Data/Interfaces/IUsuarioRepository.cs
@@ -0,0 +1,18 @@
+﻿using SistemaBancario.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaBancario.Infra.Data.Interfaces
+{
+    internal interface IUsuarioRepository
+    {
+        Task<Usuario> Inclui(Usuario usuario);
+        Task<Usuario> Alterar(Usuario usuario);
+        Task<Usuario> Excluir(int id);
+        Task<Usuario> SelecionarAsync(int id);
+        Task<IEnumerable<Usuario>> SelecionarTodosAsync();
+    }
+}
