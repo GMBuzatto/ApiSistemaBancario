34f2c26afeee023ac6dd39bc54cbd7ee1351f8ea
gabriel.buzatto <gabrielmartinbuzatto1999@gmail.com>
2023-11-04 15:34:11 -0300

    adiciona instancia do método static addInfrastructure que busca as injeções de dependencia e string de conexão

--- SistemaBancario.API/Program.cs
+++ SistemaBancario.API/Program.cs
@@ -1,3 +1,5 @@
+using SistemaBancario.Infra.Ioc;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+//método AddInfrastructure que contem nossas injeções de dependencia,
+//nossa string de conexão e migrations
+DependencyInjection.AddInfrastructure(builder);
+
 
 var app = builder.Build();
 
