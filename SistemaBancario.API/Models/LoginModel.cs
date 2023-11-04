c2a3c100e5e555bfd0091fb7bd4bd643b9ba9904
gabriel.buzatto <gabrielmartinbuzatto1999@gmail.com>
2023-11-04 20:16:50 -0300

    adiciona classe LoginModel no projeto API

---
+++ SistemaBancario.API/Models/LoginModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SistemaBancario.API.Models
+{
+    public class LoginModel
+    {
+        [Required(ErrorMessage = "O e-mail é obrigatorio")]
+        [DataType(DataType.EmailAddress)]//notation já vai verificar pra gente se é um email valido
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "O password é obrigatorio")]
+        [DataType(DataType.Password)]//notation já vai verificar pra gente se é um password valido
+        public string Password { get; set; }
+    }
+}
